7b8754eac6bcaef7d92872028ddaded0e46d3608
mideesh <mideeshp@gmail.com>
2018-10-10 12:47:03 +0530

    Fabric integrated

--- app/src/main/java/com/bitrisetesting/mideesh/bitrisetesting/MainActivity.kt
+++ app/src/main/java/com/bitrisetesting/mideesh/bitrisetesting/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bitrisetesting.mideesh.bitrisetesting
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 }
